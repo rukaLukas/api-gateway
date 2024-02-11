86b9b44c4b131a63d9551fd7c7c54e30936068fd
Lucas <lukas.figueiredo@hotmail.com>
2024-02-11 13:08:28 -0300

    133. Adicionar o SpringDoc OpenAPI UI ao API Gateway

--- src/main/java/com/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/apigateway/ApiGatewayApplication.java
@@ -2,12 +2,31 @@ package com.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@EnableDiscoveryClient
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "API Gateway", version = "1.0", description = "Documentation API Gateway v1.0"))
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+		return builder
+			.routes()
+			.route(r -> r.path("/book-service/v3/api-docs").and().method(HttpMethod.GET).uri("lb://book-service"))			
+			.route(r -> r.path("/cambio-service/v3/api-docs").and().method(HttpMethod.GET).uri("lb://cambio-service"))
+			.build();
 	}
 
 }
